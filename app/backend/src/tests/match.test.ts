ebdde3eca4f8baa618b1f81c20e6ac65212961c5
Paulo Henrique <ph46163835@gmail.com>
2022-10-27 22:42:29 -0300

    test: om valores igual do time, retorne erro com o status 401

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -35,11 +35,46 @@ describe('', async () => {
       expect(response.status).to.be.equal(200);
     });
 });
-const validUserDatabase = {
-  email: 'admin@admin.com',
-  password: 'secret_admin',
+
+const newMatchEqualTeams = {
+  homeTeam: 1,
+  homeTeamGoals: 1,
+  awayTeam: 1,
+  awayTeamGoals: 4,
 }
 
+describe('', async () => {
+  let requestHttp: Response; let response: Response;
+  beforeEach(async () => {
+    requestHttp = await chai.request(app).post('/login').send(validUser());
+    response = await chai.request(app).post('/matches')
+    .set('authorization', requestHttp.body.token).send(matchEquals());
+    sinon.stub(MatchModel, 'findOne').resolves(matchEquals() as MatchModel)
+  });
+
+  afterEach(() => (MatchModel.findOne as sinon.SinonStub).restore());
+  
+  it('"POST/macth", Com valores igual do time, retorne erro com o status 401', async () => {
+    expect(response.body).to.be.deep.equal({ message: 'There is no team with such id!' });
+    expect(response.status).to.be.equal(401);
+  });
+});
+
+function matchEquals () {
+ return {
+   homeTeam: 2,
+   homeTeamGoals: 1,
+   awayTeam: 2,
+   awayTeamGoals: 4,
+  }
+}
+
+function validUser () {
+  return {
+  email: 'admin@admin.com',
+  password: 'secret_admin'
+  }
+}
 
 function match () {
     return [
