3126bbe1144ae9b672370f30b77fb10ddcdedc8d
Paulo Henrique <ph46163835@gmail.com>
2022-11-01 21:14:45 -0300

    refactor

--- app/backend/src/database/services/query/home_query.ts
+++ app/backend/src/database/services/query/home_query.ts
@@ -5,23 +5,23 @@ FORMAT((classificação.totalPoints) / (classificação.totalGames * 3) * 100, 2
 FROM (SELECT 
 team_name as "name",
 (SUM(case 
-when mt.home_team_goals > mt.away_team_goals then 3 
-when mt.home_team_goals = mt.away_team_goals then 1
+when mth.home_team_goals > mth.away_team_goals then 3 
+when mth.home_team_goals = mth.away_team_goals then 1
 else 0 end)) as "totalPoints",
-COUNT(mt.home_team) as "totalGames",
-SUM(case when mt.home_team_goals > mt.away_team_goals then 1 else 0 end) as "totalVictories",
-SUM(case when mt.home_team_goals = mt.away_team_goals then 1 else 0 end) as "totalDraws",
-SUM(mt.home_team_goals) as "goalsFavor",
-SUM(mt.away_team_goals) as "goalsOwn",
-SUM(case when mt.home_team_goals < mt.away_team_goals then 1 else 0 end) as "totalLosses",
-SUM(mt.home_team_goals) - SUM(mt.away_team_goals) as "goalsBalance"
-FROM TRYBE_FUTEBOL_CLUBE.matches as mt 
-JOIN TRYBE_FUTEBOL_CLUBE.teams as t ON mt.home_team = t.id
+COUNT(mth.home_team) as "totalGames",
+SUM(case when mth.home_team_goals > mth.away_team_goals then 1 else 0 end) as "totalVictories",
+SUM(case when mth.home_team_goals = mth.away_team_goals then 1 else 0 end) as "totalDraws",
+SUM(mth.home_team_goals) as "goalsFavor",
+SUM(mth.away_team_goals) as "goalsOwn",
+SUM(case when mth.home_team_goals < mth.away_team_goals then 1 else 0 end) as "totalLosses",
+SUM(mth.home_team_goals) - SUM(mth.away_team_goals) as "goalsBalance"
+FROM TRYBE_FUTEBOL_CLUBE.matches as mth 
+JOIN TRYBE_FUTEBOL_CLUBE.teams as t ON mth.home_team = t.id
 WHERE in_progress = 0
-GROUP BY mt.home_team) as classificação
-ORDER BY classificação.totalPoints desc,
-classificação.goalsBalance desc ,
-classificação.goalsFavor desc,
-classificação.goalsOwn desc;`;
+GROUP BY mth.home_team) as leaderboard
+ORDER BY leaderboard.totalPoints desc,
+leaderboard.goalsBalance desc ,
+leaderboard.goalsFavor desc,
+leaderboard.goalsOwn desc;`;
 
 export default homeLeaderboard;
